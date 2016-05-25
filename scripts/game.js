57d4849586a378276b82b1e0fc7eb611e47c231d
Vivian Nguyen <vtnguyen94@gmail.com>
2016-05-24 18:50:36 -0700

    [game.js] starting game state

--- scripts/game.js
+++ scripts/game.js
@@ -7,9 +7,28 @@ var Game = function() {
 Game.prototype.initialize_game = function() {
   // (1) initialize the pool of types
   // two sets of each colors, and two special jokers red and black
+  all_tiles = []
+  for (i = 0; i < Tile.MAX_TILE_NO; i++) {
+    for (j = 0; j < Tile.COLORS.length; j++) {
+      all_tiles.push(new Tile(Tile.COLORS[j], i + 1))
+    }
+  }
 
+  // two sets
+  this.pool = all_tiles.concat(all_tiles);
+  // jokers, -1 to indicate joker
+  this.pool.append(new Tile("black", -1));
+  this.pool.append(new Tile("red", -1));
 
   // (2) create players and draws tiles for them
+  // FIXME: this is wrong...
+  this.players = [new Player(1), new Player(2)];
+  for (i = 0; i < this.players.length; i++) {
+    p = this.players[i];
+    for (j = 0; j < Game.STARTING_NO_OF_TILES; j++) {
+      this.drawTile(p);
+    }
+  }
 }
 
 Game.prototype.addToSet = function(tile) {
@@ -27,8 +46,14 @@ Game.prototype.resetBoard = function() {
 Game.prototype.changeTurns = function() {
 };
 
-Game.prototype.drawTile = function() {
+Game.prototype.drawTile = function(player) {
+  rand_index = Math.floor(Math.random() * this.pool.length)
+  var tile = this.pool[rand_index];
+  this.pool.splice(index, 1);
+  player.hand.push(tile)
 };
+
+Game.STARTING_NO_OF_TILES = 14
 
 var Board = function() {
   // has sets
@@ -37,11 +62,12 @@ var Board = function() {
 Board.prototype.checkValidity = function() {
 };
 
-var Player = function() {
+var Player = function(id) {
   // hand of tiles
   this.hand = [];
   // if init turn, must put down 30+ total
   this.initialTurn = true;
+  this.id = id
 };
 
 Player.prototype.endTurn = function() {
@@ -72,6 +98,11 @@ Set.prototype.checkValidity = function() {
 var Tile = function(color, number) {
   this.color = color;
   this.number = number;
+  this.isJoker = false;
   this.owner = undefined;
+
 }
+
+Tile.MAX_TILE_NO = 13;
+Tile.TILE_COLORS = ["blue", "orange", "red", "yellow"]
 
