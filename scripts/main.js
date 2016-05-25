2b594fbaac93269c5a995c523f61984450ecd814
Vivian Nguyen <vtnguyen94@gmail.com>
2016-05-25 16:36:37 -0700

    whoosh

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+var BoardView = React.createClass({
+  render: function() {
+    return (
+    );
+  }
+})
+
+var Hand = React.createClass({
+  render: function() {
+    return (
+    );
+  }
+})
+
+var Tile = React.createClass({
+  render: function() {
+    return (
+    );
+  }
+})
+
+var Set = React.createClass({
+  // render the set of tiles
+  render: function() {
+    return (
+    );
+  }
+})
+
+ReactDOM.render(
+  <BoardView />,
+  document.getElementById('content')
+);
+
