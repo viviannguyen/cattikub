849c5ac3103bea34980c5f1fe042f8c6d7589010
Vivian Nguyen <vtnguyen94@gmail.com>
2016-05-30 12:40:51 -0700

    [game.js] moved logic to game object -run through a game

--- scripts/game.js
+++ scripts/game.js
@@ -1,26 +1,33 @@
-
+// Game object.  Contains the board, players, pool of tiles.
+// Arguments: None
+// Return: Game object
 var Game = function() {
   this.pool = [];
   this.board = new Board;
 };
 
-Game.prototype.initialize_game = function() {
-  // (1) initialize the pool of types
-  // two sets of each colors, and two special jokers red and black
+// Initialize the game.
+// (1) initialize the pool of types:
+//    two sets of each colors, and two special jokers red and black
+// (2) create players and draws tiles for them
+// (3) Set the current player (currPlayer), save the state (savedBoard, savedHand)
+// Arguments: None.
+// Return: removed tile.
+Game.prototype.initializeGame = function() {
   all_tiles = []
   for (i = 0; i < Tile.MAX_TILE_NO; i++) {
-    for (j = 0; j < Tile.COLORS.length; j++) {
-      all_tiles.push(new Tile(Tile.COLORS[j], i + 1));
+    for (j = 0; j < Tile.TILE_COLORS.length; j++) {
+      all_tiles.push(new Tile(Tile.TILE_COLORS[j], i + 1));
     }
   }
 
   // two sets
   this.pool = all_tiles.concat(all_tiles);
-  // jokers, -1 to indicate joker
-  this.pool.append(new Tile("black", -1));
-  this.pool.append(new Tile("red", -1));
 
-  // (2) create players and draws tiles for them
+  // jokers, -1 to indicate joker
+  this.pool.push(new Tile("black", 0));
+  this.pool.push(new Tile("red", 0));
+
   // FIXME: this is wrong...
   this.players = [new Player(0), new Player(1)];
   for (i = 0; i < this.players.length; i++) {
@@ -30,79 +37,204 @@ Game.prototype.initialize_game = function() {
     }
   }
   // save the current state before a turn starts
-  this.savedBoard = this.board;
+  this.savedBoard = new Board();
   this.currPlayer = this.players[0];
-  this.savedHand = this.currPlayer.hand;
+  this.savedHand = this.currPlayer.hand.slice(0);
 }
 
-Game.prototype.addToSet = function(set, tile) {
-  set.add(tile);
+// Create a set and add it to the board.
+// Arguments: Tile.
+// Return: True.
+Game.prototype.createSet = function(tile) {
+  new_set = new gameSet([tile]);
+  g.board.sets.push(new_set);
+  return true;
 };
 
-Game.prototype.removeFromSet = function(set, tile) {
-  set.remove(tile);
-};
-
-Game.prototype.createNewSet = function(tiles) {
-  this.board.push(new Set(tiles));
-};
-
+// Reset the board to the previous state (the board and hand).
+// Arguments: None.
+// Return: True.
 Game.prototype.resetBoard = function() {
   this.board = this.savedBoard;
   this.currPlayer.hand = this.savedHand;
+  return true;
 };
 
-Game.prototype.changeTurns = function() {
-  if (this.checkWin(this.currPlayer)) {
-    this.endGame(this.currPlayer);
+// End a players turn.
+// Arguments: None
+// Return: true if changed turns, false if unvalidated
+Game.prototype.endTurn = function() {
+  isValid = g.board.checkValidity();
+  if (isValid) {
+    this.changeTurns(false);
+    return true;
   } else {
-    nextIndex = this.players.indexOf(this.currPlayer) + 1;
-    this.currPlayer = this.players[nextIndex];
-  }
+    throw "Can't end turn.  Invalid state"
+  };
 };
 
+// Change the turns from player to player.
+// (1) If it's the first turn, make sure the tiles put down add up to 30.
+// (2) Check for the win condition.
+// (3) Save state, change turn.
+// Arguments: If player skipped or not.
+// Return: removed tile.
+Game.prototype.changeTurns = function(skipped) {
+  if (this.currPlayer.initialTurn && !skipped) {
+    // take board - saved board = tiles put down, sum left over
+    boardTiles = this.savedBoard.flatten();
+    newTiles = $(this.board.flatten()).not(boardTiles).get();
+    tots = newTiles.reduce( (prev, curr) => prev + curr.number, 0);
+    if (tots < 30) {
+      alert("Tiles put down is less than 30");
+    } else {
+      this.currPlayer.initialTurn = false;
+    }
+  } else if (this.checkWin()) {
+    this.endGame(this.currPlayer);
+    return;
+  }
+  nextIndex = (this.players.indexOf(this.currPlayer) + 1) % this.players.length;
+  this.currPlayer = this.players[nextIndex];
+  this.savedBoard = Board.copy(this.board.sets);
+  this.savedHand = this.currPlayer.hand.slice(0);
+};
+
+// FIX ME
+// Does something when the game ends
+// Arguments:
+// Return:
 Game.prototype.endGame = function() {
   // end the game?!!
 }
 
-Game.prototype.checkWin = function(player) {
-  return player.hand.length == 0:
+// Checks for the win condition (when there's nothing left in the players hand).
+// Arguments: player
+// Return: true or false
+Game.prototype.checkWin = function() {
+  return this.currPlayer.hand.length == 0;
 }
 
+// Remove tile from the current players hand.
+// Arguments: Tile to be removed
+// Return: Tile removed
+Game.prototype.removeFromHand = function(tile) {
+  index = this.currPlayer.hand.indexOf(tile);
+  this.currPlayer.hand.splice(index, 1);
+  return tile;
+}
+
+// Add a tile to the players hand.
+// Arguments: player
+// Return: None
 Game.prototype.drawTile = function(player) {
   rand_index = Math.floor(Math.random() * this.pool.length);
-  var tile = this.pool[rand_index];
+  var tile = this.pool.splice(rand_index, 1)[0];
   tile.owner = player;
-  this.pool.splice(index, 1);
   player.hand.push(tile);
 };
 
-Game.prototype.skipTurn = function() {
-  this.drawTile(this.currPlayer);
-  this.changeTurns();
+Game.prototype.drawTileNonrandom = function(player) {
+  var tile = this.pool.splice(0, 1)[0];
+  tile.owner = player;
+  player.hand.push(tile);
 };
 
-Player.prototype.penalty = function() {
+// Skip the current players turn.
+// (1) Draw a tile.
+// (2) Switch turns.
+// Arguments: None
+// Return: None
+Game.prototype.skipTurn = function() {
+  this.drawTile(this.currPlayer);
+  this.changeTurns(true);
+};
+
+// Penalty is the draw 3 tiles.
+// Arguments: None
+// Return: None
+Game.prototype.penalty = function() {
   for (i = 0; i < Game.PENALTY; i++) {
     this.drawTile(this.currPlayer);
   }
-  this.changeTurns();
+  this.changeTurns(true);
 };
 
-Game.prototype.checkTurn = function(player) {
-  return this.currPlayer == player;
-};
+Game.PENALTY = 3;
+Game.STARTING_NO_OF_TILES = 14;
 
-Game.PENALTY = 3
-Game.STARTING_NO_OF_TILES = 14
-
+// Represents the game board that has all the sets on it.
+// Arguments: None
+// Return: Board object
 var Board = function() {
-  // has sets
+  this.sets = [];
 }
 
+// Make a new board given set.  Used to make a deep copy of original board to save state.
+// Arguments: Sets
+// Return: Board object
+Board.copy = function(sets) {
+  b = new Board();
+  b.sets = sets;
+  return b;
+}
+
+// Check the validity of all the sets on the board.
+// Arguments: None.
+// Return: if the board is valid
 Board.prototype.checkValidity = function() {
+  for (i = 0; i < this.sets.length; i++) {
+    if (!this.sets[i].checkValidity()) {
+      return false;
+    }
+  }
+  return true;
 };
 
+// Flatten all tiles into a list of tiles.
+// Arguments: None.
+// Return: List of all tiles on the board.
+Board.prototype.flatten = function() {
+  all_tiles = [];
+  for (i=0; i < this.sets.length; i++) {
+    all_tiles = all_tiles.concat(this.sets[i].tiles);
+  }
+  return all_tiles;
+};
+
+// Add tile to the tile list and update counters.
+// Arguments: Tile to add, 0 for beginning, 1 for end.
+// Returns: Nothing.
+Board.prototype.addTile = function(set, tile, end){
+  if (end) {
+    set.tiles.push(tile);
+  } else {
+    set.tiles.unshift(tile);
+  }
+  set.numCount[tile.number] += 1;
+  set.colorCount[tile.color] += 1;
+}
+
+// Remove tile of matching color and number from tile list and update counters, 
+// Arguments: Tile to remove, 0 for beginning, 1 for end.
+// Return: removed tile.
+Board.prototype.removeTile = function(set, tile){
+  var i = set.indexOf(a);
+  // split it into 2 sets and return removed tile
+  var before_set = set.tiles.slice(0, i)
+  var later_set = set.tiles.slice(i)
+  var removed = later_set.splice(0, 1)[0];
+  // remove the old set and add the new sets to the board
+  i_of_set = this.sets(set);
+  this.sets.splice(i_of_set, 0);
+  this.sets.push(before_set);
+  this.sets.push(later_set);
+  return removed;
+}
+
+// Create player object, has a hand and id.
+// Arguments: id
+// Return: Player object
 var Player = function(id) {
   // hand of tiles
   this.hand = [];
@@ -111,17 +243,10 @@ var Player = function(id) {
   this.id = id;
 };
 
-Player.prototype.endTurn = function() {
-  isValid = Board.checkValidity();
-  if (isValid) {
-    Game.prototype.changeTurns();
-  } else {
-    // red shadow around wrong sets
-    // yellow shadow around player's tiles
-  };
-};
-
-var Set = function(tiles) {
+// Represents a game set on the board.
+// Arguments: tiles to make up a set
+// Return: Game set object
+var gameSet = function(tiles) {
   // 3+ tiles
   // run set: run of the same color
   // group set: group of same number, but different colors
@@ -147,7 +272,7 @@ var Set = function(tiles) {
 // and check for gaps in the sequence, and whether or not a joker is used.
 // Arguments: None.
 // Returns: a boolean.
-Set.prototype.checkValidity = function() {
+gameSet.prototype.checkValidity = function() {
   if(this.tiles.length < 3){
     return false
   }
@@ -206,38 +331,66 @@ function onesAndZeros(counter){
   return [zeros, ones];
 }
 
-// Add tile to the tile list and update counters.
-// Arguments: Tile to add.
-// Returns: Nothing.
-Set.prototype.addTile = function(tile){
-  this.tiles.push(tile);
-  this.numCount[tile.number] += 1;
-  this.colorCount[tile.color] += 1;
-}
-
-// Remove tile of matching color and number from tile list and update counters, 
-// Arguments: Tile to remove.
-// Returns: bool of whether the tile was removed or not.
-Set.prototype.removeTile = function(tile){
-  for(var i = 0; i < this.tiles.length; i++){
-    if(tile.color === this.tiles[i].color && tile.number == this.tiles[i].number){
-      this.tiles.splice(i, 1);
-      this.numCount[tile.number] -= 1;
-      this.colorCount[tile.color] -= 1;
-      return true;
-    }
-  }
-  return false;
-}
-
+// Tile object, has a color, number, owner.
+// Arguments: None
+// Return: Tile object
 var Tile = function(color, number) {
   this.color = color;
   this.number = number;
-  this.isJoker = false;
   this.owner = undefined;
 
 }
 
 Tile.MAX_TILE_NO = 13;
-Tile.TILE_COLORS = ["blue", "orange", "red", "black"]
+Tile.TILE_COLORS = ["blue", "orange", "red", "black"];
+
+// Log an array of tiles.
+// Arguments: Array of tiles.
+// Return: String of tile info.
+Game.logTiles = function(hand) {
+ h = "";
+  for (i=0; i < hand.length; i++) {
+    tile = hand[i].color + ": " + hand[i].number + ", ";
+    h += tile;
+  }
+  return h;
+}
+
+// Test a new game
+g = new Game
+all_tiles = []
+for (i = 0; i < Tile.TILE_COLORS.length; i++) {
+  for (j = 0; j < Tile.MAX_TILE_NO; j++) {
+    all_tiles.push(new Tile(Tile.TILE_COLORS[i], j + 1));
+  }
+}
+// two sets
+g.pool = all_tiles.concat(all_tiles);
+
+// jokers, -1 to indicate joker
+g.pool.push(new Tile("black", 0));
+g.pool.push(new Tile("red", 0));
+
+g.players = [new Player(0), new Player(1)];
+for (i=0; i < g.players.length; i++) {
+  for (j=0; j<14; j++) {
+    g.drawTileNonrandom(g.players[i]);
+  }
+}
+
+// save the current state before a turn starts
+g.savedBoard = new Board();
+g.currPlayer = g.players[0];
+g.savedHand = g.currPlayer.hand;
+t = g.currPlayer.hand[0];
+g.removeFromHand(t);
+g.createSet(t);
+for (i=0; i<10; i++) {
+  t = g.currPlayer.hand[0];
+  g.removeFromHand(t);
+  g.board.addTile(g.board.sets[0], t, 1);
+}
+
+g.endTurn();
+g.skipTurn();
 
